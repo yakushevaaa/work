b8ba0cd4e8af15908f160602097bde7f782923db
Владислава Якушева <muzisnvlada@gmail.com>
2024-04-11 21:00:29 +0300

    test commit

---
+++ tetst proj/tetst proj/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace tetst_proj
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
